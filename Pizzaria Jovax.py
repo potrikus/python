a7f2c7cc1d821f21e052dbfe2e63d9a15dce004e
potrikus <fabriciobergman@hotmail.com>
2025-07-27 14:34:40 -0300

    Update Pizzaria Jovax.py

--- Pizzaria Jovax.py
+++ Pizzaria Jovax.py
@@ -1,34 +1,40 @@
 opçao = ''
+lista = []
 while opçao != '1' and opçao != '2':
-    print ('SEJA BEM VINDO PIZZARIA JOVAX!')
+    print('SEJA BEM VINDO PIZZARIA JOVAX!')
     opçao = input('Digite 1 para ir para o cardapio:').strip()
     if opçao == '1':
-        
-                        # Aqui começa janela de cardapio:
-        finalizar = ''# '1'para pedido.
-        cardapio = ['camarao','quatro queijos','peperone','frango']
-        pedido = ['']
+
+        # Aqui começa janela de cardapio:
+        finalizar = ''  # '1'para pedido.
+        cardapio = ['camarao', 'quatro queijos', 'peperone', 'frango']
+
         while finalizar != '1':
-            print ('         CARDAPIO DO DIA!')
-            print ('')
-            print ('Faça seu pedido e digite 1 para finalizar:')
-            print ('')
-            print ('Pizza de camarao: R$ 70.00')
-            print ('Pizza de quatro queijos R$ 50.00')
-            print ('Pizza de peperone R$ 50.00')
-            print ('Pizza de frango R$ 50.00')
+            print('         CARDAPIO DO DIA!')
+            print('')
+            print('Faça seu pedido e digite 1 para finalizar:')
+            print('')
+            print('Pizza de camarao: R$ 70.00')
+            print('Pizza de quatro queijos R$ 50.00')
+            print('Pizza de peperone R$ 50.00')
+            print('Pizza de frango R$ 50.00')
+
             pedido = input('Pizza de:').strip().lower()
-            if pedido in cardapio:
-                print ('                     ',pedido)
-        
-        
-        
-        
-        
-        
-        
-        
+            if pedido == '1':
+               finalizar = '1'
+            elif pedido in cardapio:
+               lista.append(pedido)
+            else:
+              print ('Item nao encontrado!')
+            print(lista)
+
+
+
+
+
+
+
     elif opçao == '2':
-        print ('FINALIZANDO SISTEMA!')
+         print('FINALIZANDO SISTEMA!')
     else:
-        print ('ENTRADA INVALIDA!')
+        print('ENTRADA INVALIDA!')
